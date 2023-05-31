96c201adc6482de5749051a59c87b45d59b3b7aa
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-05-31 18:15:05 +0100

    too hard i'll get back to this

---
+++ Easy/118. Pascal's Triangle/Solution.java
@@ -0,0 +1,16 @@
+import java.sql.ClientInfoStatus;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        List<Integer> integers = new ArrayList<>();
+
+
+        return result;
+    }
+}
+
+//(n+1)C(r) = (n)C(r - 1) + (n)C(r)
