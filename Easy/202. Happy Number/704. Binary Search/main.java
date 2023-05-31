a803b44ab7a2f9f097d437b12b3248da79694d76
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-05-31 14:40:41 +0100

    d

--- Easy/202. Happy Number/704. Binary Search/main.java
+++
@@ -1,7 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-
-        System.out.println(solution.search(new int[]{-1,0,3,5,9,12},9));
-    }
-}
