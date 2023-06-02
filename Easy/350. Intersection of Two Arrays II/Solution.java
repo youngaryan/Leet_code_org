e660f094e2651226f57e804f4e4445b3b962f909
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-06-02 12:45:59 +0100

    similar to what i was trying to do yesterday

--- Easy/350. Intersection of Two Arrays II/Solution.java
+++ Easy/350. Intersection of Two Arrays II/Solution.java
@@ -1,18 +1,26 @@
-import java.util.*;
+import java.util.ArrayList;
+
+import static java.util.Arrays.sort;
+
 public class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        List<Integer> re = new ArrayList<>();
+        ArrayList<Integer> re = new ArrayList<>();
+        int j=0, k=0;
+        sort(nums1);
+        sort(nums2);
 
-        for (int i = 0; i < nums1.length; i++) {
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums1[i] == nums2[j]) {
-                    re.add(nums1[i]);
-                    nums1[i] = -1;
-                    nums2[j] = -2;
-                }
+        while (j<nums1.length &&k<nums2.length){
+            if (nums1[j] < nums2[k])
+                j++;
+            else if (nums1[j] > nums2[k]) {
+                k++;
+            }
+            else {
+                re.add(nums1[j]);
+                j++;
+                k++;
             }
         }
-
 
         int [] result = new int[re.size()];
         for (int i = 0; i < re.size(); i++)
