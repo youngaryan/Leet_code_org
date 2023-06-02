c4269385308259b03280dbbdef248f07ddca6eae
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-06-02 16:06:50 +0100

    done

---
+++ Easy/27. Remove Element/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.removeElement(new int[]{3,2,2,3},3));
+    }
+}
