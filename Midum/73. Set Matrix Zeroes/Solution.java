f07628802bc453dae2d6bdba5dcc6f8130002024
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-06-08 19:37:49 +0100

    will come back to this

---
+++ Midum/73. Set Matrix Zeroes/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public void setZeroes(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0){
+
+                }
+            }
+        }
+    }
+}
