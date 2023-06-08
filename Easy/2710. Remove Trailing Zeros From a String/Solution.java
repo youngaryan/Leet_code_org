2b29fdc745d83ec229a99484d4e994e3de5040a2
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-06-08 14:04:15 +0100

    2ms i think i should jave used for loops insted

---
+++ Easy/2710. Remove Trailing Zeros From a String/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public String removeTrailingZeros(String num) {
+
+        int i = num.length()-1;
+        while (num.charAt(i) == '0'){
+            i--;
+        }
+
+        return num.substring(0,i);
+    }
+}
