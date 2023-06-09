9e1c2c4a76068fb53c3033e3a9616bfd2baaa965
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-06-09 09:50:04 +0100

    will come back to ot

---
+++ Easy/1071. Greatest Common Divisor of Strings/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if (str2.length()>str1.length())return "";
+        String re  = str2;
+        int j = 0, i = 0;
+       while (i<str1.length() )
+        return re;
+    }
+}
---
+++ Easy/1071. Greatest Common Divisor of Strings/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.gcdOfStrings("LEET","CODE"));
+        //"ABCABC","ABC"
+//        "ABABAB","ABAB"
+    }
+}
