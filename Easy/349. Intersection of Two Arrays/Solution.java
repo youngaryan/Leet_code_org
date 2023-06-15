29c76fd98bbf1cafe5ddba69ed85582fc455da02
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-06-15 18:16:57 +0100

    2ms

--- Easy/349. Intersection of Two Arrays/Solution.java
+++ Easy/349. Intersection of Two Arrays/Solution.java
@@ -1,33 +1,23 @@
-import java.util.ArrayList;
-
+import java.util.HashSet;
 public class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        ArrayList<Integer> arrayList1 = new ArrayList<>();
 
+        HashSet<Integer> s1 = new HashSet<>();
+        HashSet<Integer> s2 = new HashSet<>();
 
-        if (nums1.length>nums2.length) {
-            for (int k : nums1) {
-                arrayList.add(k);
-            }
-            for (int j : nums2) {
-                if (arrayList.contains(j) && !arrayList1.contains(j)) arrayList1.add(j);
-            }
-        }else {
-            for (int k : nums2) {
-                arrayList.add(k);
-            }
-            for (int j : nums1) {
-                if (arrayList.contains(j) && !arrayList1.contains(j)) arrayList1.add(j);
-            }
+        for (int n : nums1)
+            s1.add(n);
+
+        for (int n : nums2) {
+            if (s1.contains(n)) s2.add(n);
         }
 
-        int[] re = new int[arrayList1.size()];
-        int i = 0;
-        while (!arrayList1.isEmpty()){
-            re[i] = arrayList1.get(0);
-            arrayList1.remove(0);
-            i++;
+        int[] re = new int[s2.size()];
+        int j = 0;
+
+        for (int n : s2){
+            re[j] = n;
+            j++;
         }
         return re;
     }
