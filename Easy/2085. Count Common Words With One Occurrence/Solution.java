5df8d84bb321f834cba2e668da12fb66f896fcb2
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-06-16 21:44:45 +0100

    4ms

--- Easy/2085. Count Common Words With One Occurrence/Solution.java
+++ Easy/2085. Count Common Words With One Occurrence/Solution.java
@@ -1,40 +1,32 @@
 import java.util.HashSet;
+import java.util.Set;
 
-public class Solution {
+class Solution {
     public int countWords(String[] words1, String[] words2) {
-        HashSet<String> s1 = new HashSet<>();
-        HashSet<String> s2 = new HashSet<>();
+        Set<String> usedOnceA = new HashSet<>();
+        Set<String> usedMoreThanOnce = new HashSet<>();
+        Set<String> usedOnceAandB = new HashSet<>();
 
-        HashSet<String> remove1 = new HashSet<>();
-
-        for(String s : words1) {
-            if (!s1.contains(s) && !remove1.contains(s)) {
-                s1.add(s);
-
-            } else {
-                s1.remove(s);
-                remove1.add(s);
+        for (String w : words1) {
+            if (usedOnceA.contains(w)) {
+                usedOnceA.remove(w);
+                usedMoreThanOnce.add(w);
+            }
+            if (!usedMoreThanOnce.contains(w)) {
+                usedOnceA.add(w);
             }
         }
-
-
-        System.out.println(s1);
-        remove1.clear();
-        System.out.println(remove1);
-        for(String st : words2){
-            if(s1.contains(st) && !remove1.contains(st)){
-                s2.add(st);
-                System.out.println(s2);
-            }else {
-                try {
-                    s2.remove(st);
-                    remove1.add(st);
-                }catch (Exception e){}
+        for (String w : words2) {
+            if (usedOnceA.contains(w)) {
+                if (usedOnceAandB.contains(w)) {
+                    usedOnceAandB.remove(w);
+                    usedMoreThanOnce.add(w);
+                }
+                if (!usedMoreThanOnce.contains(w)) {
+                    usedOnceAandB.add(w);
+                }
             }
         }
-
-        System.out.println(s2);
-        return s2.size();
+        return usedOnceAandB.size();
     }
 }
-
