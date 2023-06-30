e0e0b8578e1d6fb832ef4f0e0f8879fe3017f221
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-06-30 13:58:56 +0100

    4ms

--- Easy/1071. Greatest Common Divisor of Strings/Main.java
+++ Easy/1071. Greatest Common Divisor of Strings/Main.java
@@ -1,8 +1,10 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.gcdOfStrings("LEET","CODE"));
+        System.out.println(s.gcdOfStrings("ABABABABA", "ABAB"));
         //"ABCABC","ABC"
 //        "ABABAB","ABAB"
+//        "LEET","CODE"
+        //"AA", "A"
     }
 }
--- Easy/1071. Greatest Common Divisor of Strings/Solution.java
+++ Easy/1071. Greatest Common Divisor of Strings/Solution.java
@@ -1,9 +1,33 @@
 public class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        if (str2.length()>str1.length())return "";
-        String re  = str2;
-        int j = 0, i = 0;
-       while (i<str1.length() )
-        return re;
+        int gcd = gcd(str1.length(),str2.length());
+
+
+        for (int i = 0; i < str2.length(); i++) {
+            if (str2.charAt(i) != str2.charAt(i%gcd)){
+                return "";
+            }
+        }
+
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) != str2.charAt(i%gcd) )return "";
+        }
+
+        return str2.substring(0,gcd);
+    }
+
+    private static int gcd(int x, int y) {
+        {
+            int r , a, b;
+            a = Math.max(x, y);
+            b = Math.min(x, y);
+            r = b;
+            while (a % b != 0) {
+                r = a % b;
+                a = b;
+                b = r;
+            }
+            return r;
+        }
     }
 }
