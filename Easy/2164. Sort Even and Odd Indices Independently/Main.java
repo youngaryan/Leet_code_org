b72e5e3d9c810c349afcd9d411ced194e19ea402
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-07-01 17:25:51 +0100

    2ms

---
+++ Easy/2164. Sort Even and Odd Indices Independently/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(Arrays.toString(s.sortEvenOdd(new int[]{4,1,2,3})));
+
+        //5,39,33,5,12,27,20,45,14,25,32,33,30,30,9,14,44,15,21
+    }
+}
--- Easy/2164. Sort Even and Odd Indices Independently/Solution.java
+++ Easy/2164. Sort Even and Odd Indices Independently/Solution.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Solution {
+import static java.util.Arrays.sort;
+
+public class Solution {
+    public int[] sortEvenOdd(int[] nums) {
+        int j = 0, k = 0;
+        int[] oddIndexes = new int[nums.length/2];
+
+        int[] evenIndexes = new int[nums.length - oddIndexes.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i%2 == 0){
+                evenIndexes[j] = nums[i];
+                j++;
+            }else {
+                oddIndexes[k] = nums[i];
+                k++;
+            }
+        }
+
+        sort(oddIndexes);
+        sort(evenIndexes);
+
+        j = 0;
+        k = oddIndexes.length-1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i%2 == 0){
+                nums[i] = evenIndexes[j];
+                j++;
+            }else {
+                nums[i] = oddIndexes[k];
+                k--;
+            }
+        }
+
+        return nums;
+    }
 }
