2bbec42ebc85a0aceaaa7a274c88ccd4fcd84136
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-07-19 17:44:00 +0100

    49ms

--- Midum/15. 3Sum/Solution.java
+++ Midum/15. 3Sum/Solution.java
@@ -6,23 +6,38 @@ public class Solution
     {
         if (nums.length <3)return new ArrayList<>();
 
-        Set<List<Integer>> setRes = new HashSet<>();
+        Set<List<Integer>> res = new HashSet<>();
+        Arrays.sort(nums);
 
-        for (int i = 0; i < nums.length-2; i++) {
-            for (int j = 1; j < nums.length-1; j++) {
-                for (int k = 2; k < nums.length; k++) {
-                    List<Integer> integers = new ArrayList<>();
-                    if (nums[i]+nums[j]+nums[k] == 0  && i!=j && i!=k && j!=k){
-                        integers.add(nums[i] );
-                        integers.add(nums[j] );
-                        integers.add(nums[k] );
-                        Collections.sort(integers);
-                        setRes.add(integers);
+        for (int i = 0; i < nums.length; i++) {
+            if (i>0 && nums[i] == nums[i-1]){
+                continue;
+            }
+
+            int target = -nums[i];
+            int left = i+1;
+            int right = nums.length-1;
+
+            while (left<right){
+                int sum = nums[left]+nums[right];
+
+                if (sum == target){
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+
+                    while (left<right && nums[left] == nums[left-1]){
+                        left++;
+                    }while (left<right && nums[right] == nums[right+1]){
+                        right--;
                     }
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
                 }
             }
         }
-
-        return setRes.stream().toList();
+        return new ArrayList<>(res);
     }
 }
