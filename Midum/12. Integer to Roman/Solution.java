9c2ca4b29ce8e3ee141ad221a99314f3483c323e
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-07-25 17:00:34 +0100

    will come back nher

---
+++ Midum/12. Integer to Roman/Solution.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Solution
+ */
+public class Solution {
+
+    public String intToRoman(int num) {
+        
+        Map<String, Integer> map = new HashMap<>();
+
+        map.put("I", 1);
+        map.put("I", 1); 
+        map.put("I", 1);
+        map.put("I", 1);
+        map.put("I", 1); 
+        map.put("I", 1);
+
+        return "";
+    }
+}
