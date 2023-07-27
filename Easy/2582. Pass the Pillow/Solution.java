9d7ce88cd502a2c4890a7461b4c7532cd1a17a23
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-07-27 23:31:45 +0100

    beats 100%

--- Easy/2582. Pass the Pillow/Solution.java
+++ Easy/2582. Pass the Pillow/Solution.java
@@ -2,25 +2,25 @@ class Solution {
     public  int passThePillow(int n, int time) {
         if(time == 0) return 1;
 
+        boolean toggle = false;
         int current = 1;
 
-        while (time != 0 ){
-            current++;
-            time--;
-            if(current == n){
-                current = otherway(current, time);
-            
+        while(time != 0){
+            while(toggle == false && time !=0){
+                current++;
+                time--;
+                if(current == n){
+                    toggle = true;
+                }
+            }while(toggle == true && time != 0){
+                current--;
+                time--;
+                if(current == 1){
+                    toggle = false;
+                }
             }
         }
-        return current;
-    }
 
-    private int otherway(int current, int time) {
-        while( current != 1){
-            current --;
-            time--;
-            if(time == 0) break;
-        }
         return current;
     }
 }
