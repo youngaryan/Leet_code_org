b4c5cd55282e61b5858f775c412bb39c442062e0
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-07-30 11:44:47 +0100

    delete main

--- Easy/1281. Subtract the Product and Sum of Digits of an Integer/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Solution s = new Solution();
-
-        System.out.println(s.subtractProductAndSum(243));
-    
-        System.out.println("Hello World!");
-    }
-}
