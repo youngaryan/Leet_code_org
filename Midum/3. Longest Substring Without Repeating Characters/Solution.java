3d67069d3cadfe4a3d9df21f4f76472067e48651
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-07-31 21:59:24 +0100

    amazing 7ms

--- Midum/3. Longest Substring Without Repeating Characters/Solution.java
+++ Midum/3. Longest Substring Without Repeating Characters/Solution.java
@@ -1,48 +1,23 @@
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.HashSet;
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if(s.length() == 1) return 1;
-
-        int max = 0;
-        List<String> tokens = split(s);
-        // System.out.println(tokens);
-        
-        for (String string : tokens) {
-            if(string.length() > max) max = string.length();
-        }
-        return max;
-    }
+        HashSet<Character> hashSet = new HashSet<>();
+        int length = s.length() , left = 0, max = 0;
+        char tempRight;
 
 
-
-    private List<String> split(String s) {
-        List<String> tStrings = new ArrayList<>();
-        List<Character> temCharacters = new ArrayList<>();
-        StringBuilder stringBuilder = new StringBuilder();
-        char e;
-        int left= 0 ,right = 1;
-
-        while(right != s.length()){
-            e = s.charAt(right);
-            if(temCharacters.contains(e)){
-                tStrings.add(stringBuilder.substring(left, right+1).toString());
-                stringBuilder.delete(left, left+1);
-                temCharacters.remove(e);
-                left ++;
+        for (int right = left; right < length; right++) {
+            tempRight = s.charAt(right);
+            while(hashSet.contains(tempRight)){
+                hashSet.remove(s.charAt(left));
+                left++;
             }
-            temCharacters.add(e);
-            stringBuilder.append(e);
-            right++;
-            // System.out.println("tStrings " + tStrings);
-            // System.out.println("tempChatr "+ temCharacters);
-            // System.out.println("sculider " +stringBuilder);
+            hashSet.add(tempRight);
+            
+            if(hashSet.size() > max) max = hashSet.size();
         }
-        tStrings.add(stringBuilder.toString());
-    
 
-        return tStrings;
+        return max;
     }
 }
