b01997660735a897b43ed28a71873427288f222e
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-08-08 11:47:53 +0100

    1ms

--- Easy/1961. Check If String Is a Prefix of Array/Solution.java
+++ Easy/1961. Check If String Is a Prefix of Array/Solution.java
@@ -2,15 +2,11 @@ class Solution {
     public boolean isPrefixString(String s, String[] words) {
         if(s.length() < words[0].length()){ return false;}
         StringBuilder stringBuilder = new StringBuilder();
-
-        for (int i = 0; i < words.length; i++) {
-            stringBuilder.append(words[i]); 
-        }
         
-        if(s.length() > stringBuilder.length()) return false;
-        for (int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) != stringBuilder.charAt(i)) return false;
+        for (int i = 0; i < words.length; i++) {
+            stringBuilder.append(words[i]);
+            if(stringBuilder.toString().equals(s)) return true;
         }
-        return true;
+        return false;
     }
 }
