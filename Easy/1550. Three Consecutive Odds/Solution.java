e54f9f1202c56691462b9f91c13d5bf12b9b5349
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-08-09 12:01:53 +0100

    formatter

--- Easy/1550. Three Consecutive Odds/Solution.java
+++ Easy/1550. Three Consecutive Odds/Solution.java
@@ -3,10 +3,11 @@ class Solution {
         int count = 0;
 
         for (int i : arr) {
-            if(i%2 != 0){
-                count ++;
-                if(count == 3)return true;
-            }else{
+            if (i % 2 != 0) {
+                count++;
+                if (count == 3)
+                    return true;
+            } else {
                 count = 0;
             }
         }
