eafae121d938f91f45028204ce5dfd69c217949c
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-08-09 09:25:03 +0100

    0ms

---
+++ Easy/1550. Three Consecutive Odds/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count = 0;
+
+        for (int i : arr) {
+            if(i%2 != 0){
+                count ++;
+                if(count == 3)return true;
+            }else{
+                count = 0;
+            }
+        }
+        return false;
+    }
+}
