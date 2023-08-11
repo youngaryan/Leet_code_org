75784cdb5427577f2469a4027a1220befbc29e76
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-08-11 10:29:48 +0100

    1ms

---
+++ Easy/771. Jewels and Stones/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+
+        for (int i = 0; i < stones.length(); i++) {
+            if (jewels.contains(String.valueOf(stones.charAt(i)))) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
