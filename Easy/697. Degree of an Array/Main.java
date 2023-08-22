45a98b17ac038efa823883d18554182e8044c3a6
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-08-22 19:33:08 +0100

    still trying

--- Easy/697. Degree of an Array/Main.java
+++ Easy/697. Degree of an Array/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
 
-        System.out.println(s.findShortestSubArray(new int[] { 1, 1 }));
+        System.out.println(s.findShortestSubArray(new int[] { 1, 2, 2, 3, 1 }));
     }
 }
--- Easy/697. Degree of an Array/Solution.java
+++ Easy/697. Degree of an Array/Solution.java
@@ -1,23 +1,27 @@
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
     public int findShortestSubArray(int[] nums) {
-        if (nums.length == 1)
-            return nums[0];
-        Arrays.sort(nums);
-        int j = 0, max = -1, current = -1;
+        Map<Integer, Integer> map = new HashMap<>();
+        int current = -1, max = -1;
 
-        for (int i = j; i < nums.length; i++) {
-            if (nums[i] != nums[j]) {
-                if (max < i - j) {
-                    max = i - j;
-                    current = nums[j];
-                }
-                j = i;
-                i = j + 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
             }
         }
 
-        return current == -1 ? nums.length : current * max;
+        for (int i = 0; i < nums.length; i++) {
+            if (max <= map.get(nums[i])) {
+                current = nums[i];
+                max = map.get(nums[i]);
+            }
+        }
+
+        System.out.println(current);
+        return max;
     }
 }
