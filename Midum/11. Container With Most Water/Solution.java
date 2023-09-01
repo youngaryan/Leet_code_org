a08bd81200783a92cd6b383d1e7acb3dc4c6b5dc
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-09-01 17:25:01 +0100

    just tried to brute force, it works, but not effi

--- Midum/11. Container With Most Water/Solution.java
+++ Midum/11. Container With Most Water/Solution.java
@@ -1,35 +1,14 @@
-import java.util.Arrays;
-
 class Solution {
     public int maxArea(int[] height) {
-        int hor1 = 0, hor2 = 0, max = Integer.MIN_VALUE, length = height.length;
-        int[] ver = Arrays.copyOf(height, length);
-        Arrays.sort(ver);
-
-        // first find the maxVer
-        // then find the second, third max and soforth
-        // and compare how mauch water they contain
+        int max = Integer.MIN_VALUE, length = height.length, min;
 
         for (int i = 0; i < length; i++) {
-            if (ver[length - 1] == height[i]) {
-                hor1 = i;
-                break;
+            for (int j = 0; j < length; j++) {
+                min = Math.min(height[j], height[i]);
+                max = Math.max(max, min * (i - j));
             }
         }
-
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < length - 1; j++) {
-                if (ver[j] == height[i]) {
-                    hor2 = i;
-                    max = Math.max(max, Math.abs(hor1 - hor2) * ver[j]);
-                }
-            }
-        }
-
-        max = Math.max(max, (length - 1) * ver[0]);
 
         return max;
     }
 }
-
-// [1,2,4,3]
