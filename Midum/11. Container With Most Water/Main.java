d26248072dfcfc62a67412a0037f515ee946d3aa
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-09-01 16:36:43 +0100

    i've not considered case which the largest el not

---
+++ Midum/11. Container With Most Water/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.maxArea(new int[] { 1, 2, 1 }));
+        // 1, 8, 6, 2, 5, 4, 8, 3, 7
+        // 1, 1
+        // 1, 2, 1
+    }
+}
---
+++ Midum/11. Container With Most Water/Solution.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+class Solution {
+    public int maxArea(int[] height) {
+        int hor1 = 0, hor2 = 0, max = Integer.MIN_VALUE, length = height.length;
+        int[] ver = Arrays.copyOf(height, length);
+        Arrays.sort(ver);
+
+        // first find the maxVer
+        // then find the second, third max and soforth
+        // and compare how mauch water they contain
+
+        for (int i = 0; i < length; i++) {
+            if (ver[length - 1] == height[i]) {
+                hor1 = i;
+                break;
+            }
+        }
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length - 1; j++) {
+                if (ver[j] == height[i]) {
+                    hor2 = i;
+                    max = Math.max(max, Math.abs(hor1 - hor2) * ver[j]);
+                }
+            }
+        }
+
+        return max;
+    }
+}
