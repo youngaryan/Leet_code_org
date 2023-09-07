a705cbca2cf788876556fbb1364e364dfb0e08e6
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-09-07 14:04:15 +0100

    1ms, first hard q

--- hard/42. Trapping Rain Water/Main.java
+++ hard/42. Trapping Rain Water/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
 
-        System.out.println(s.trap(new int[] { 4, 2, 0, 3, 2, 5 }));
+        System.out.println(s.trap(new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 }));
     }
 }
--- hard/42. Trapping Rain Water/Solution.java
+++ hard/42. Trapping Rain Water/Solution.java
@@ -3,24 +3,26 @@ class Solution {
         if (height.length == 2 || height.length == 1)
             return 0;
 
-        int maxWater = 0, maxLeft, maxRight;
+        int maxWater = 0, maxLeft = 0, maxRight = 0, leftPointer = 0, rightPointer = height.length - 1, curentWater = 0;
 
-        for (int i = 0; i < height.length; i++) {
-            maxLeft = 0;
-            maxRight = 0;
-            for (int j = 0; j <= i; j++) {
-                if (height[j] > maxLeft) {
-                    maxLeft = height[j];
+        while (leftPointer < rightPointer) {
+            if (height[leftPointer] < height[rightPointer]) {
+                if (height[leftPointer] > maxLeft) {
+                    maxLeft = height[leftPointer];
                 }
+                curentWater = maxLeft - height[leftPointer];
+                leftPointer++;
+            } else {
+                if (height[rightPointer] > maxRight) {
+                    maxRight = height[rightPointer];
+                }
+                curentWater = maxRight - height[rightPointer];
+                rightPointer--;
             }
 
-            for (int j = i + 1; j < height.length; j++) {
-                if (height[j] > maxRight) {
-                    maxRight = height[j];
-                }
+            if (curentWater > 0) {
+                maxWater += curentWater;
             }
-
-            maxWater = Math.max(maxWater, (maxWater + Math.min(maxRight, maxLeft) - height[i]));
         }
 
         return maxWater;
