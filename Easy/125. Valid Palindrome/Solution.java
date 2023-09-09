296a6c09e9de3264c119621cec6ad93ff26afe65
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-09-09 13:45:08 +0100

    22ms

---
+++ Easy/125. Valid Palindrome/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        int i = 0, j = s.length() - 1;
+
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
