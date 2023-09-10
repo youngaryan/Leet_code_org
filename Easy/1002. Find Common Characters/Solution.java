be6106411dea24c36cd645951bbae7793ed59a53
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-09-10 12:28:19 +0100

    better solution, 3ms

--- Easy/1002. Find Common Characters/Solution.java
+++ Easy/1002. Find Common Characters/Solution.java
@@ -1,46 +1,32 @@
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 class Solution {
     public List<String> commonChars(String[] words) {
-        Map<Character, Integer> commonCharsMap = new HashMap<>();
-
-        for (char c : words[0].toCharArray()) {
-            commonCharsMap.put(c, commonCharsMap.getOrDefault(c, 0) + 1);
-        }
-
-        for (int i = 1; i < words.length; i++) {
-            Map<Character, Integer> currentWordMap = new HashMap<>();
-            for (char c : words[i].toCharArray()) {
-                currentWordMap.put(c, currentWordMap.getOrDefault(c, 0) + 1);
-            }
-
-            List<Character> toRemove = new ArrayList<>();
-            for (char key : commonCharsMap.keySet()) {
-                if (currentWordMap.containsKey(key)) {
-                    commonCharsMap.put(key, Math.min(commonCharsMap.get(key), currentWordMap.get(key)));
-                } else {
-                    toRemove.add(key);
-                }
-            }
-
-            for (char key : toRemove) {
-                commonCharsMap.remove(key);
-            }
-        }
-
         List<String> result = new ArrayList<>();
-        for (Map.Entry<Character, Integer> entry : commonCharsMap.entrySet()) {
-            char character = entry.getKey();
-            int frequency = entry.getValue();
 
-            for (int i = 0; i < frequency; i++) {
-                result.add(String.valueOf(character));
+        int[] minFrequencies = new int[26];
+        Arrays.fill(minFrequencies, 101);
+
+        for (String string : words) {
+            int[] currentCharCount = new int[26];
+
+            for (char c : string.toCharArray()) {
+                currentCharCount[c - 'a']++;
+            }
+
+            for (int i = 0; i < 26; i++) {
+                minFrequencies[i] = Math.min(currentCharCount[i], minFrequencies[i]);
             }
         }
 
+        for (int i = 0; i < 26; i++) {
+            while (minFrequencies[i] > 0) {
+                result.add("" + (char) (i + 'a'));
+                minFrequencies[i]--;
+            }
+        }
         return result;
     }
 }
