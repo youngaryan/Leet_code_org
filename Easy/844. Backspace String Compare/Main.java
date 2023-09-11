9501bc7b132e4c6d9a790a55675d92aabb5677e6
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-09-11 20:53:13 +0100

    unable to comeup with diff solution

--- Easy/844. Backspace String Compare/Main.java
+++ Easy/844. Backspace String Compare/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
 
-        System.out.println(s.backspaceCompare("a##c", "#a#c"));
+        System.out.println(s.backspaceCompare("bbbextm", "bbb#extm"));
 
         // "a##c"
         // "#a#c"
--- Easy/844. Backspace String Compare/Solution.java
+++ Easy/844. Backspace String Compare/Solution.java
@@ -1,34 +1,84 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        StringBuilder sList = new StringBuilder();
-        StringBuilder tList = new StringBuilder();
-        char temp;
-        int length;
+        int i = s.length() - 1, j = t.length() - 1;
 
-        for (int i = 0; i < s.length(); i++) {
-            temp = s.charAt(i);
-            if (temp == '#') {
-                length = sList.length();
-                if (length > 0) {
-                    sList.deleteCharAt(length - 1);
+        while (i >= 0 && j >= 0) {
+            if (s.charAt(i) == '#') {
+                while (i > 0 && s.charAt(i - 1) == '#') {
+                    i -= 2;
                 }
+                i -= 2;
+                continue;
+            }
+            if (j > -1 && t.charAt(j) == '#') {
+                while (t.charAt(j - 1) == '#') {
+                    j -= 2;
+                }
+                j -= 2;
+                continue;
+            }
+
+            if (s.charAt(i) != t.charAt(j)) {
+                return false;
+            }
+            i--;
+            j--;
+        }
+
+        while (i > -1) {
+            if (s.charAt(i) == '#') {
+                while (i > 0 && s.charAt(i - 1) == '#') {
+                    i -= 2;
+                }
+                i -= 2;
             } else {
-                sList.append(temp);
+                return false;
             }
         }
 
-        for (int i = 0; i < t.length(); i++) {
-            temp = t.charAt(i);
-            if (temp == '#') {
-                length = tList.length();
-                if (length > 0) {
-                    tList.deleteCharAt(length - 1);
+        while (j > -1) {
+            if (t.charAt(j) == '#') {
+                while (j > 0 && t.charAt(j - 1) == '#') {
+                    j -= 2;
                 }
+                j -= 2;
             } else {
-                tList.append(temp);
+                return false;
             }
         }
 
-        return sList.toString().equals(tList.toString());
+        return true;
+
+        // StringBuilder sList = new StringBuilder();
+        // StringBuilder tList = new StringBuilder();
+        // char temp;
+        // int length;
+
+        // for (int i = 0; i < s.length(); i++) {
+        // temp = s.charAt(i);
+        // if (temp == '#') {
+        // length = sList.length();
+        // if (length > 0) {
+        // sList.deleteCharAt(length - 1);
+        // }
+        // } else {
+        // sList.append(temp);
+        // }
+        // }
+
+        // for (int i = 0; i < t.length(); i++) {
+        // temp = t.charAt(i);
+        // if (temp == '#') {
+        // length = tList.length();
+        // if (length > 0) {
+        // tList.deleteCharAt(length - 1);
+        // }
+        // } else {
+        // tList.append(temp);
+        // }
+        // }
+
+        // return sList.toString().equals(tList.toString());
+
     }
 }
