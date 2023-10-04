3e30d0c8a3e95e6dbc4305dd9f8ce23d30595cbb
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-10-04 21:29:14 +0100

    277ms

--- Easy/706. Design HashMap/Main.java
+++ Easy/706. Design HashMap/Main.java
@@ -2,7 +2,35 @@ public class Main {
     public static void main(String[] args) {
         MyHashMap hashMap = new MyHashMap();
 
-        hashMap.put(1, 3);
-        System.out.println(hashMap.get(1));
+        hashMap.put(11, 0);
+        System.out.println(hashMap.get(11));
     }
 }
+
+/*
+ * class MyHashMap {
+ * 
+ * public MyHashMap() {
+ * 
+ * }
+ * 
+ * public void put(int key, int value) {
+ * 
+ * }
+ * 
+ * public int get(int key) {
+ * 
+ * }
+ * 
+ * public void remove(int key) {
+ * 
+ * }
+ * }
+ * 
+ * /**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
--- Easy/706. Design HashMap/MyHashMap.java
+++ Easy/706. Design HashMap/MyHashMap.java
@@ -1,55 +1,29 @@
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 class MyHashMap {
-    final int MAXCALL = 10000;
-    List<Integer>[] buckets;
+
+    int[] hashMap = new int[1000];
+    Set<Integer> keysUsed = new HashSet<>();
 
     public MyHashMap() {
-        buckets = new LinkedList[MAXCALL];
-
-        Arrays.fill(buckets, new LinkedList<>());
-    }
-
-    int hash(int key) {
-        return key % MAXCALL;
     }
 
     public void put(int key, int value) {
-        int i = hash(key);
-
-        List<Integer> bucket = buckets[i];
-        if (bucket.contains(key)) {
-            bucket.set((Integer) key, value);
-        }
-        System.out.println(buckets);
-
+        hashMap[key] = value;
+        keysUsed.add(key);
     }
 
     public int get(int key) {
-        int i = hash(key);
-
-        List<Integer> bucket = buckets[i];
-        if (bucket.contains(key)) {
-            System.out.println(buckets);
-
-            return bucket.get((Integer) key);
+        if (hashMap[key] != 0 || keysUsed.contains(key)) {
+            return hashMap[key];
         }
-        System.out.println(buckets);
-
         return -1;
     }
 
     public void remove(int key) {
-
-        int i = hash(key);
-
-        List<Integer> bucket = buckets[i];
-        if (bucket.contains(key)) {
-            bucket.remove((Integer) key);
-        }
-        System.out.println(buckets);
+        hashMap[key] = 0;
+        keysUsed.remove(key);
     }
 }
 
