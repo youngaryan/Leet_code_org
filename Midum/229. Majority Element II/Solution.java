9fe20a0f51c0259c0381166391be03a05ce47e7b
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-10-05 15:21:50 +0100

    2ms

--- Midum/229. Majority Element II/Solution.java
+++ Midum/229. Majority Element II/Solution.java
@@ -1,23 +1,46 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer, Integer> count = new HashMap<>();
-
-        for (int i : nums) {
-            count.put(i, count.getOrDefault(i, 0) + 1);
-        }
-
+        int count1 = 0, count2 = 0, maj1 = 0, maj2 = 0;
         List<Integer> result = new ArrayList<>();
 
-        for (int key : count.keySet()) {
-            if (count.get(key) > nums.length / 3) {
-                result.add(key);
+        for (int i : nums) {
+            if (i == maj1) {
+                count1++;
+            } else if (i == maj2) {
+                count2++;
+            } else if (count1 == 0) {
+                maj1 = i;
+                count1++;
+            } else if (count2 == 0) {
+                maj2 = i;
+                count2++;
+            } else {
+                count1--;
+                count2--;
             }
         }
 
+        count1 = 0;
+        count2 = 0;
+
+        for (int i : nums) {
+            if (i == maj1) {
+                count1++;
+            } else if (i == maj2) {
+                count2++;
+            }
+        }
+
+        if (count1 > nums.length / 3) {
+            result.add(maj1);
+        }
+
+        if (count2 > nums.length / 3) {
+            result.add(maj2);
+        }
         return result;
     }
 }
