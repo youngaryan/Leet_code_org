419e8b1ba00581cc0472a30d752b43c9f1693fa3
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-10-06 15:46:43 +0100

    1ms

--- Easy/2206. Divide Array Into Equal Pairs/Solution.java
+++ Easy/2206. Divide Array Into Equal Pairs/Solution.java
@@ -1,25 +1,16 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
     public boolean divideArray(int[] nums) {
-
-        Set<Integer> seen = new HashSet<>();
+        int[] hold = new int[501];
 
         for (int i = 0; i < nums.length; i++) {
-            int count = 0;
-            if (seen.add(nums[i])) {
-                for (int j = 0; j < nums.length; j++) {
-                    if (nums[i] == nums[j]) {
-                        count++;
-                    }
-                }
-            }
-            if (count % 2 != 0) {
+            hold[nums[i]]++;
+        }
+
+        for (int i : hold) {
+            if (i % 2 != 0) {
                 return false;
             }
         }
-
         return true;
     }
 }
