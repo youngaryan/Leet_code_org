134c3ae72e84c34256c1963a7d96fbee6329a375
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-10-09 18:46:28 +0100

    1ms

--- Easy/1876. Substrings of Size Three with Distinct Characters/Solution.java
+++ Easy/1876. Substrings of Size Three with Distinct Characters/Solution.java
@@ -1,20 +1,13 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
     public int countGoodSubstrings(String s) {
         int count = 0;
-        Set<Character> set = new HashSet<>();
 
         for (int i = 0; i < s.length() - 2; i++) {
-            set.clear();
-            for (int j = i; j < i + 3; j++) {
-                set.add(s.charAt(j));
-                if (set.size() != j - i + 1) {
-                    break;
-                } else if (set.size() == 3) {
-                    count++;
-                }
+            String string = s.substring(i, i + 3);
+            char[] c = string.toCharArray();
+
+            if (c[0] != c[1] && c[2] != c[1] && c[2] != c[0]) {
+                count++;
             }
         }
 
