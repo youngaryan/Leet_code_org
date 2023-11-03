00d146d27157914a67681e66e69b2b7233213070
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-11-03 18:04:29 +0000

    1ms

---
+++ Midum/56. Merge Intervals/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().merge(new int[][] {
+                { 1, 4 },
+                { 4, 5 }
+        }));
+    }
+}
