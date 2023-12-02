ddcf57340da607f6c8f532376727f159f48d6f63
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-12-02 14:46:38 +0000

    0ms

---
+++ Easy/112. Path Sum/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.left == null && root.right == null) {
+            return root.val == targetSum;
+        }
+
+        boolean left = hasPathSum(root.left, targetSum - root.val);
+        boolean right = hasPathSum(root.right, targetSum - root.val);
+
+        return left || right;
+    }
+}
