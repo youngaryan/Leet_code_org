cfae2766e14c04981614ad8bb5bd541e248b0091
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-12-09 10:25:32 +0000

    5ms

--- Easy/812. Largest Triangle Area/Solution.java
+++ Easy/812. Largest Triangle Area/Solution.java
@@ -1,18 +1,12 @@
 class Solution {
     public double largestTriangleArea(int[][] points) {
         double max = -1;
-        int[] firstSide = new int[2], secondSide = new int[2], thirdSide = new int[2];
         for (int i = 0; i < points.length; i++) {
             for (int j = i+1; j < points.length; j++) {
                 for (int j2 = j+1; j2 < points.length; j2++) {
-                    firstSide[0] = points[i][0];
-                    firstSide[1] = points[i][1];
-                    secondSide[0] = points[j][0];
-                    secondSide[1] = points[j][1];
-                    thirdSide[0] = points[j2][0];
-                    thirdSide[1] = points[j2][1];
-                    if (max < findArea(firstSide, secondSide, thirdSide)) {
-                        max = findArea(firstSide, secondSide, thirdSide);
+                   
+                    if (max < findArea( points[i][0],  points[i][1], points[j][0],points[j][1], points[j2][0], points[j2][1])) {
+                        max = findArea( points[i][0],  points[i][1], points[j][0],points[j][1], points[j2][0], points[j2][1]);
                     }                
                 }
             }
@@ -20,9 +14,8 @@ class Solution {
         return max;
     }
 
-    private double findArea(int[] firstSide, int[] secondSide, int[] thirdSide) {
-        double area = 0.5 * Math.abs((firstSide[0]) * (secondSide[1] - thirdSide[1])
-                + secondSide[0] * (thirdSide[1] - firstSide[1]) + thirdSide[0] * (firstSide[1] - secondSide[1]));
+    private double findArea(int x1, int y1, int x2, int y2, int x3, int y3) {
+        double area = 0.5 * Math.abs((x1) * (y2 - y3) + (x2) * (y3 - y1) + (x3) * (y1 - y2));
         return area;
     }
 }
