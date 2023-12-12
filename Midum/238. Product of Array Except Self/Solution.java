9c2d570a0cd4bbf5a6ff73e688a754d36b2d6652
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-12-12 02:19:15 +0000

    less memory 2ms

--- Midum/238. Product of Array Except Self/Solution.java
+++ Midum/238. Product of Array Except Self/Solution.java
@@ -1,26 +1,23 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int len = nums.length, prodcut;
-        int[] result = new int[len], leftArray = new int[len], rightArray = new int[len];
+        int[] result = new int[len];
 
-        leftArray[0] = 1;
-        rightArray[len - 1] = 1;
+        result[0] = 1;
 
         prodcut = 1;
         for (int i = 1; i < len; i++) {
             prodcut *= nums[i - 1];
-            leftArray[i] = prodcut;
+            result[i] = prodcut;
         }
 
         prodcut = 1;
         for (int i = len - 2; i >= 0; i--) {
-            prodcut *= nums[i + 1];
-            rightArray[i] = prodcut;
+            prodcut *= nums[i + 1] ;
+            result[i] = prodcut*result[i];
         }
 
-        for (int i = 0; i < len; i++) {
-            result[i] = leftArray[i] * rightArray[i];
-        }
+        
         return result;
     }
 }
