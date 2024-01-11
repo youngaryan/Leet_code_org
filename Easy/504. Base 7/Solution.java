a90fefc8bd608757205891ac46b2bc6c1b01b89f
Aryan Golbaghi <aryangolbaghi@gmail.com>
2024-01-11 02:53:57 +0000

    504

--- Easy/504. Base 7/Solution.java
+++ Easy/504. Base 7/Solution.java
@@ -1,17 +1,9 @@
 public class Solution {
     public String convertToBase7(int num) {
-        //100
-        //0,7,49,343
-        if (num == 0)return "0";
-        if (num<7)return Integer.toString(num);
+        int mod = num/7;
+        int rem = num % 7;
+        
 
-        boolean negative = false;
-
-        if (num<0){num = num*-1; negative = true;}
-
-        while (num!=0){
-            int mod = num%7;
-        }
-        return null;
+        return "";
     }
 }
