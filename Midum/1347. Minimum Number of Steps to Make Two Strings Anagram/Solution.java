10f2e8498a2cb1adeea2a6f1b6d448d771e2c7b5
Aryan Golbaghi <aryangolbaghi@gmail.com>
2024-01-13 21:07:59 +0000

    8ms beats 76%

---
+++ Midum/1347. Minimum Number of Steps to Make Two Strings Anagram/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minSteps(String s, String t) {
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        int r = 0;
+
+        for (int i : count) {
+            if (i > 0) {
+                r+=i;
+            }
+        }
+
+        return r;
+    }
+}
