10e9218078313ff167ae223a298095cb10edf483
Aryan Golbaghi <aryangolbaghi@gmail.com>
2024-01-19 21:50:35 +0000

    greddy approach dont' work!

---
+++ Midum/931. Minimum Falling Path Sum/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().minFallingPathSum(new int[][] {
+                { 100, -42, -46,
+                        -41 },
+                {
+                        31, 97, 10, -10
+                },
+                { -58, -51, 82,
+                        89 },
+                { 51, 81, 69,
+                        -51 }
+        }));
+    }
+}
+// [[100,-42,-46,-41],[31,97,10,-10],[-58,-51,82,89],[51,81,69,-51]]
