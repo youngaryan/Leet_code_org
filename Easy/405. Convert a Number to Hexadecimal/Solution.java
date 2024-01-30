a554e9882413fadf7c880c2c4b1240d138eed27d
Aryan Golbaghi <aryangolbaghi@gmail.com>
2024-01-30 21:18:02 +0000

    1

--- Easy/405. Convert a Number to Hexadecimal/Solution.java
+++ Easy/405. Convert a Number to Hexadecimal/Solution.java
@@ -2,27 +2,14 @@ import java.util.HashMap;
 
 public class Solution {
     public String toHex(int num) {
+        HashMap<Integer, Character> map = new HashMap<>();
+        map.put(10, 'A');
+        map.put(11, 'B');
+        map.put(12, 'C');
+        map.put(13, 'D');
+        map.put(14, 'E');
+        map.put(15, 'F');
 
-        if (num<0)return toHex((int) Math.pow(16,8) + num);
-
-
-        StringBuilder re = new StringBuilder();
-        HashMap<Integer, String> hashMap = new HashMap<>();
-        hashMap.put(10,"a");
-        hashMap.put(11,"b");
-        hashMap.put(12,"c");
-        hashMap.put(13,"d");
-        hashMap.put(14,"e");
-        hashMap.put(15,"f");
-        while (num>0){
-            if (num % 16 < 10){
-                re.append(num % 16);
-            }else {
-                re.append(hashMap.get(num%16));
-            }
-            num = num/16;
-        }
-
-        return re.reverse().toString();
+        return "";
     }
 }
