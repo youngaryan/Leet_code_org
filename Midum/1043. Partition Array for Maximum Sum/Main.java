603b201312477952203f05e98f3fd284eeba9668
Aryan Golbaghi <aryangolbaghi@gmail.com>
2024-02-03 22:31:07 +0000

    colun't

---
+++ Midum/1043. Partition Array for Maximum Sum/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().maxSumAfterPartitioning(new int[] { 1, 15, 7, 9, 2, 5, 10 }, 3));
+    }
+}
