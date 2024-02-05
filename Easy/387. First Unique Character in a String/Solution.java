c87950d58e3df878863463c1b162ae26a7e57d94
Aryan Golbaghi <aryangolbaghi@gmail.com>
2024-02-05 11:29:24 +0000

    6ms

--- Easy/387. First Unique Character in a String/Solution.java
+++ Easy/387. First Unique Character in a String/Solution.java
@@ -1,39 +1,18 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
     public int firstUniqChar(String s) {
-        Set<Integer> integers = new HashSet<>();
-        Set<Integer> duplicates = new HashSet<>();
+        int[] integers = new int[26];
 
         for (int i = 0; i < s.length(); i++) {
-            if (!integers.add(s.charAt(i) - 'a')) {
-                duplicates.add(s.charAt(i) - 'a');
-            }
+            integers[s.charAt(i) - 'a']++;
         }
 
         for (int i = 0; i < s.length(); i++) {
-            if (!duplicates.contains(s.charAt(i) - 'a')) {
+
+            if (integers[s.charAt(i) - 'a'] == 1) {
                 return i;
             }
+
         }
         return -1;
     }
 }
-
-/*
- * Example 1:
- * 
- * Input: s = "leetcode"
- * Output: 0
- * 
- * Example 2:
- * 
- * Input: s = "loveleetcode"
- * Output: 2
- * 
- * Example 3:
- * 
- * Input: s = "aabb"
- * Output: -1
- */
