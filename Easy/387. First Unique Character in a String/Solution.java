0e7272f27f9e46b266f6f297ebf53db0a9d5ee6f
Aryan Golbaghi <aryangolbaghi@gmail.com>
2024-02-05 11:23:37 +0000

    27ms still bad

--- Easy/387. First Unique Character in a String/Solution.java
+++ Easy/387. First Unique Character in a String/Solution.java
@@ -1,30 +1,39 @@
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 class Solution {
     public int firstUniqChar(String s) {
-        List<Character> c = new ArrayList<>();
-        Set<Character> exist = new HashSet<>();
-        char w;
-        
+        Set<Integer> integers = new HashSet<>();
+        Set<Integer> duplicates = new HashSet<>();
 
         for (int i = 0; i < s.length(); i++) {
-            w = s.charAt(i);
-            if(c.contains(w)){
-                exist.add(w);
+            if (!integers.add(s.charAt(i) - 'a')) {
+                duplicates.add(s.charAt(i) - 'a');
             }
-            c.add(w);
         }
 
-        for (int i = 0; i < c.size(); i++) {
-            w = c.get(i);
-
-            if(!exist.contains(w)){
+        for (int i = 0; i < s.length(); i++) {
+            if (!duplicates.contains(s.charAt(i) - 'a')) {
                 return i;
             }
         }
         return -1;
     }
 }
+
+/*
+ * Example 1:
+ * 
+ * Input: s = "leetcode"
+ * Output: 0
+ * 
+ * Example 2:
+ * 
+ * Input: s = "loveleetcode"
+ * Output: 2
+ * 
+ * Example 3:
+ * 
+ * Input: s = "aabb"
+ * Output: -1
+ */
