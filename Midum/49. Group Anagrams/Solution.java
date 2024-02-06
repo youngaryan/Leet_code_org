6c7659345277d964d372189e3d827c45c674f345
Aryan Golbaghi <aryangolbaghi@gmail.com>
2024-02-06 16:50:58 +0000

    6ms

--- Midum/49. Group Anagrams/Solution.java
+++ Midum/49. Group Anagrams/Solution.java
@@ -1,57 +1,32 @@
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        int[][] mat = new int[strs.length][26];
+        Map<String, List<String>> map = new HashMap<>();
 
         for (int i = 0; i < strs.length; i++) {
-            for (int j = 0; j < strs[i].length(); j++) {
-                mat[i][strs[i].charAt(j) - 'a']++;
+            char[] tmp = strs[i].toCharArray();
+
+            Arrays.sort(tmp);
+
+            String key = new String(tmp);
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
             }
+
+            map.get(key).add(strs[i]);
         }
 
         List<List<String>> result = new ArrayList<>();
-        ArrayList<String> temp;
-        Set<Integer> included = new HashSet<>();
-        boolean check;
 
-        for (int i = 0; i < mat.length; i++) {
-            if (included.contains(i)) {
-                continue;
-            }
-
-            temp = new ArrayList<>();
-            temp.add(strs[i]);
-            included.add(i);
-
-            for (int j = i + 1; j < mat.length; j++) {
-                check = true;
-                for (int j2 = 0; j2 < 26; j2++) {
-                    // find all anagram for each
-                    if (mat[i][j2] != mat[j][j2]) {
-                        check = false;
-                        break;
-                    }
-
-                }
-                if (check) {
-                    temp.add(strs[j]);
-                    included.add(j);
-                }
-
-            }
-            result.add(temp);
-
+        for (List<String> list : map.values()) {
+            result.add(list);
         }
-        // System.out.println(Arrays.toString(mat[0]));
-        // System.out.println(Arrays.toString(mat[1]));
-
-        // System.out.println(Arrays.deepToString(mat));
-        // System.out.println(mat.length);
-        // System.out.println(result);
         return result;
     }
 }
