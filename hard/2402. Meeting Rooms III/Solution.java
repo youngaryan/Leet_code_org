a5ed04cd8cb34b8ecf22f871bd8331f9974c49f2
Aryan Golbaghi <aryangolbaghi@gmail.com>
2024-02-19 15:38:48 +0000

    changed the int to long fails 80/82

--- hard/2402. Meeting Rooms III/Solution.java
+++ hard/2402. Meeting Rooms III/Solution.java
@@ -6,30 +6,31 @@ class Solution {
     public int mostBooked(int n, int[][] meetings) {
         Arrays.sort(meetings, Comparator.comparingInt(a -> a[0]));
 
-        PriorityQueue<Integer> pqAvail = new PriorityQueue<>();
-        for (int i = 0; i < n; i++) {
+        PriorityQueue<Long> pqAvail = new PriorityQueue<>();
+        for (long i = 0; i < n; i++) {
             pqAvail.offer(i);
         }
 
         long[] count = new long[n];
-        PriorityQueue<int[]> pqEndAndRoom = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        PriorityQueue<long[]> pqEndAndRoom = new PriorityQueue<>(
+                (a, b) -> a[0] != b[0] ? Long.compare(a[0], b[0]) : Long.compare(a[1], b[1]));
 
         for (int i = 0; i < meetings.length; i++) {
 
             while (!pqEndAndRoom.isEmpty() && meetings[i][0] >= pqEndAndRoom.peek()[0]) {
-                pqAvail.offer(pqEndAndRoom.poll()[1]);
+                pqAvail.offer((long)(pqEndAndRoom.poll()[1]));
             }
 
             if (pqAvail.isEmpty()) {
-                int[] t = pqEndAndRoom.poll();
-                meetings[i][1] = t[0] + (meetings[i][1] - meetings[i][0]);
+                long[] t = pqEndAndRoom.poll();
+                meetings[i][1] = (int)t[0] + (meetings[i][1] - meetings[i][0]);
 
-                pqAvail.offer(t[1]);
+                pqAvail.offer((long)(t[1]));
 
             }
-            int room = pqAvail.poll();
-            pqEndAndRoom.offer(new int[] { meetings[i][1], room });
-            count[room]++;
+            long room = pqAvail.poll();
+            pqEndAndRoom.offer(new long[] { meetings[i][1], room });
+            count[(int) room]++;
 
         }
 
