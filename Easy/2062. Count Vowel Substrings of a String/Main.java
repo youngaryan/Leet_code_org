be3ce13e235b51799eb7a8903e584043e75234ce
Aryan Golbaghi <aryangolbaghi@gmail.com>
2024-03-04 15:38:14 +0000

    14ms

--- Easy/2062. Count Vowel Substrings of a String/Main.java
+++ Easy/2062. Count Vowel Substrings of a String/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Solution().countVowelSubstrings("aeiouu"));
+        System.out.println(new Solution().countVowelSubstrings("cuaieuouac"));
     }
 }
 // aeiouu
--- Easy/2062. Count Vowel Substrings of a String/Solution.java
+++ Easy/2062. Count Vowel Substrings of a String/Solution.java
@@ -1,86 +1,27 @@
+import java.util.HashSet;
+
 class Solution {
     public int countVowelSubstrings(String word) {
-        // HashSet<Character> characters = new HashSet<>();
+        int count = 0, len = word.length();
+        HashSet<Character> set = new HashSet<>();
 
-        // characters.add('a');
-        // characters.add('e');
-        // characters.add('u');
-        // characters.add('i');
-        // characters.add('o');
-        int left = 0, right = 4, count = 0;
-        word+='2';
+        for (int i = 0; i < len - 4; i++) {
+            set.clear();
 
-        while (left < right && right <= word.length()) {
+            for (int j = i; j < len; j++) {
+                if (word.charAt(j) == 'a' || (word.charAt(j)) == 'e' || (word.charAt(j)) == 'i'
+                        || (word.charAt(j) == 'o' || (word.charAt(j) == 'u'))) {
+                    set.add(word.charAt(j));
 
-            while (left < right && !(isVowel(word.charAt(left)))) {
-                left++;
-                continue;
-            }
-
-            while (left + 4 > right) {
-                right++;
-                continue;
-            }
-
-            // System.out.println("left : " + left + ", right :" + right);
-            // System.out.println(word.substring(left, right));
-            // System.out.println(includeAllVowels(word, left, right));
-            // System.out.println("------------------");
-            if (includeAllVowels(word, left, right)) {
-                count++;
-                right++;
-                continue;
-            }
-            right++;
-            if (right == word.length() && left + 4 != word.length()) {
-                left++;
-                right = left + 4;
+                    if (set.size() == 5) {
+                        count++;
+                    }
+                } else {
+                    break;
+                }
             }
         }
         return count;
     }
 
-    private boolean includeAllVowels(String w, int left, int right) {
-        boolean[] vowel = new boolean[5];
-        for (int i = left; i <= right; i++) {
-            if (w.charAt(i) == 'a') {
-                vowel[0] = true;
-                continue;
-            }
-            if (w.charAt(i) == 'e') {
-                vowel[1] = true;
-                continue;
-            }
-            if (w.charAt(i) == 'i') {
-                vowel[2] = true;
-                continue;
-            }
-            if (w.charAt(i) == 'o') {
-                vowel[3] = true;
-                continue;
-            }
-            if (w.charAt(i) == 'u') {
-                vowel[4] = true;
-                continue;
-            }
-
-            return false;
-
-        }
-
-        for (boolean c : vowel) {
-            if (c == false) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private boolean isVowel(char c) {
-        if (c == 'a' || c == 'e' || c == 'u' || c == 'i' || c == 'o') {
-            return true;
-        }
-
-        return false;
-    }
 }
