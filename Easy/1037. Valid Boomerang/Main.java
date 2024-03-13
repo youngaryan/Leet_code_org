810ef329699525638009e750cd3e1bad456df4b7
Aryan Golbaghi <aryangolbaghi@gmail.com>
2024-03-13 17:02:22 +0000

    0ms

---
+++ Easy/1037. Valid Boomerang/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().isBoomerang(new int[][] {
+                { 0,2 },
+                { 0, 1 },
+                { 0, 1 }
+        }));
+    }
+}
