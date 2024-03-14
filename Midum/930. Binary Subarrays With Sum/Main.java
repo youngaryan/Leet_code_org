1e0180fa26ed0e1056f5139558fa40e77e08efe4
Aryan Golbaghi <Aryangolbaghi@gmail.com>
2024-03-14 15:21:06 +0000

    intresting way of doing it, found it online 1ms

---
+++ Midum/930. Binary Subarrays With Sum/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // System.out.println(new Solution().numSubarraysWithSum(new int[] { 1, 0, 1, 0, 1 }, 2));
+        System.out.println(new Solution().numSubarraysWithSum(new int[] {0,0,0,0,0 }, 0));
+
+    }
+}
